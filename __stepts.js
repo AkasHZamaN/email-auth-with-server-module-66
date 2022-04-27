0444f929cef5ddb1446f00f2b6a51424d9d08108
AkasHZamaN <akashzaman4@gmail.com>
2022-04-27 12:23:30 +0600

    stepts added

--- __stepts.js
+++ __stepts.js
@@ -1,7 +1,14 @@
 /**
- * ONE TIME
+ * ONE TIME FOR YOUR COMPUTER
+ * ---------------------------
  * 1. create heroku accoutn
  * 2. verify email
  * 3. install heroku cli
  * 4. heroku login
+ * ----------------------
+ * FOR EACH PROJECT ONE TIME
+ * ------------------------
+ * 1. heroju create
+ * 2. 
+ * 
  */
